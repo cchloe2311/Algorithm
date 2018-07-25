3520a76b6d5f63a4589543fd6aca674f136ab1ab
cchloe2311 <cchloe2311@daum.net>
2018-07-25 02:14:03 -0400

    fixed mistake

--- stage16/1003.cpp
+++ stage16/1003.cpp
@@ -14,12 +14,12 @@ int main() {
         if(test[i] > test[maxIndex]) maxIndex = i;
     }
 
-    pair<int, int> *d = new pair<int, int>[test[numOfTestCase-1]+1];
+    pair<int, int> *d = new pair<int, int>[test[maxIndex]+1];
     d[0] = make_pair(1, 0);
     d[1] = make_pair(0, 1);
 
     for(int i=2; i<= test[maxIndex]; i++)
-        d[i] = make_pair(d[i-2].first+d[i-1].first, d[i-2].second + d[i-1].second);
+        d[i] = make_pair(d[i-2].first + d[i-1].first, d[i-2].second + d[i-1].second);
 
     for(int i=0; i<numOfTestCase; i++)
         cout << d[test[i]].first <<  " " << d[test[i]].second << endl;
