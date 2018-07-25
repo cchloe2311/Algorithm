7b9d8189a27731b24306a9181801d5dac94e0e0f
cchloe2311 <cchloe2311@daum.net>
2018-07-25 14:38:24 -0400

    #1149 fixed compile error

--- stage16/1149.cpp
+++ stage16/1149.cpp
@@ -17,29 +17,28 @@ int main(){
     m.insert(make_pair(1, make_pair(0, 2)));
     m.insert(make_pair(2, make_pair(0, 1)));
 
-    int **costTable = new int*[3];
-    int **d = new int*[3];
-    for(int i=0; i<3; i++) {
-        costTable[i] = new int[numOfTestCase]; // costTable[3][numOfTable]
-        d[i] = new int[numOfTestCase];
+    int **costTable = new int*[numOfTestCase];
+    long **d = new long*[numOfTestCase];
+    for(int i=0; i<numOfTestCase; i++) {
+        costTable[i] = new int[3]; // costTable[3][numOfTable]
+        d[i] = new long[3];
     }
 
     for(int i=0; i<numOfTestCase; i++)
-        cin >> costTable[0][i] >> costTable[1][i] >> costTable[2][i];
-// 좌표가 잘못됐음
+        for(int j=0; j<3; j++) cin >> costTable[i][j];
 
     d[0][0] = costTable[0][0];
-    d[1][0] = costTable[1][0];
-    d[2][0] = costTable[2][0];
+    d[0][1] = costTable[0][1];
+    d[0][2] = costTable[0][2];
 
     for(int i=1; i<numOfTestCase; i++)
         for(int j=0; j<3; j++)
-            d[j][i] = min(d[m.find(j)->second.first][i-1]+costTable[j][i],
-                          d[m.find(j)->second.second][i-1]+costTable[j][i]);
+            d[i][j] = min(d[i-1][m.find(j)->second.first]+costTable[i][j],
+                          d[i-1][m.find(j)->second.second]+costTable[i][j]);
 
-    int minValue = INT_MAX;
+    long minValue = 1000001;
     for(int i=0; i<3; i++)
-        if(minValue > d[i][numOfTestCase-1]) minValue = d[i][numOfTestCase-1];
+        if(minValue > d[numOfTestCase-1][i]) minValue = d[numOfTestCase-1][i];
 
     cout << minValue;
 }
