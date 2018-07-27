a0d19e3191898b5cafb549aad41f53d754a0d649
cchloe2311 <cchloe2311@daum.net>
2018-07-27 00:49:58 -0400

    #10844 it occurs overflow on n = 33

---
+++ stage16/10844.cpp
@@ -0,0 +1,40 @@
+//
+// Created by 최서정 on 2018. 7. 27..
+//
+
+#include <iostream>
+using namespace std;
+
+#define MOD 1000000000
+
+int main(){
+    long long factor[2][10] = { 0, };
+
+    factor[0][0] = 1;
+    factor[0][9] = 1;
+
+    int n;
+    cin >> n;
+    long long *d = new long long[n+1];
+    d[1] = 9;
+
+    for(int i=2; i<=n; i++){
+        int indexSrc =  i%2;
+        int indexDest = (i+1)%2;
+        long long sumOfFactor = 0;
+        for(int j=0; j<10; j++){
+            if((j-1)>=0) factor[indexDest][j-1] += factor[indexSrc][j];
+            if((j+1)<=9) factor[indexDest][j+1] += factor[indexSrc][j];
+        }
+        factor[indexSrc][0] = 0;
+        for(int j=1; j<10; j++) {
+            sumOfFactor += factor[indexSrc][j]%MOD;
+            factor[indexSrc][j] = 0;
+        }
+        d[i] = ((d[i-1] - sumOfFactor)*2 + sumOfFactor)%MOD;
+        if(i == 33) {
+            printf("stop\n");
+        }
+    }
+    cout << d[n]%MOD;
+}
