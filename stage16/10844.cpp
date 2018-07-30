ca4367f03e6aace885db5a083aabaa3a3d58f463
cchloe2311 <cchloe2311@daum.net>
2018-07-30 17:10:19 -0400

    #10844 solved

--- stage16/10844.cpp
+++ stage16/10844.cpp
@@ -1,6 +1,6 @@
-//
-// Created by 최서정 on 2018. 7. 27..
-//
+////
+//// Created by 최서정 on 2018. 7. 27..
+////
 
 #include <iostream>
 using namespace std;
@@ -23,18 +23,17 @@ int main(){
         int indexDest = (i+1)%2;
         long long sumOfFactor = 0;
         for(int j=0; j<10; j++){
-            if((j-1)>=0) factor[indexDest][j-1] += factor[indexSrc][j];
-            if((j+1)<=9) factor[indexDest][j+1] += factor[indexSrc][j];
+            if((j-1)>=0) factor[indexDest][j-1] = (factor[indexDest][j-1] + factor[indexSrc][j])%MOD;
+            if((j+1)<=9) factor[indexDest][j+1] = (factor[indexDest][j+1] + factor[indexSrc][j])%MOD;
         }
         factor[indexSrc][0] = 0;
         for(int j=1; j<10; j++) {
-            sumOfFactor += factor[indexSrc][j]%MOD;
+            sumOfFactor = (sumOfFactor + factor[indexSrc][j])%MOD;
             factor[indexSrc][j] = 0;
         }
-        d[i] = ((d[i-1] - sumOfFactor)*2 + sumOfFactor)%MOD;
-        if(i == 33) {
-            printf("stop\n");
-        }
+        d[i] = d[i-1] - sumOfFactor;
+        if(d[i] < 0) d[i] += MOD;
+        d[i] = (d[i]*2 + sumOfFactor)%MOD;
     }
     cout << d[n]%MOD;
 }
