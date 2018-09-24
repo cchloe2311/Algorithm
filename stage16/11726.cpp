6592e5c88bd4f2e6a7b2eb7bf20fbae9a51ad635
cchloe2311 <cchloe2311@daum.net>
2018-09-25 00:39:39 +0900

    solved #11726

---
+++ stage16/11726.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 최서정 on 2018. 9. 25..
+//
+
+#include <iostream>
+#define MOD 10007
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int *d = new int[n+1];
+    d[0] = 1;
+    d[1] = d[0];
+    if(n != 1)
+        for(int i=2; i<=n; i++)
+            d[i] = (d[i-1] + d[i-2])%MOD;
+    cout << d[n];
+}
