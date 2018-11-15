95747dbeff42c6a05c8d7e14d504aab2afc2e0b6
cchloe2311 <cchloe2311@daum.net>
2018-11-15 11:17:54 +0900

    solved beehive problem

---
+++ course3/beehive.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//
+//#define GAP 6
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    int startPoint = 1, endPoint = 1;
+//    int jump = 1, time = 1;
+//
+//    while(1){
+//        if(startPoint <= n && endPoint >= n){
+//            cout << time;
+//            return 0;
+//        }
+//        startPoint += jump;
+//        jump = GAP*time++;
+//        endPoint += jump;
+//    }
+//}
