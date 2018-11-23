58a897373dd2bfa5f767c58b17e949b86798478e
cchloe2311 <cchloe2311@daum.net>
2018-11-24 03:37:40 +0900

    solved PROSHEK problem

---
+++ course3/PROSJEK.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    long long *input = new long long[n+1];
+//    input[0] = 0;
+//
+//    for(int i=1; i<=n; i++) cin >> input[i];
+//
+//    for(int i=0; i<n; i++)
+//        cout << input[i+1]*(i+1)-input[i]*(i) <<" ";
+//}
