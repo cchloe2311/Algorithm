be385ee6a545761cbc2c57b1817a6b92082cd691
cchloe2311 <cchloe2311@daum.net>
2018-11-24 03:02:47 +0900

    solved pfactorization problem

---
+++ course3/pfactorization.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//
+//    if(n == 1){
+//        cout << 1 ;
+//        return 0;
+//    }
+//
+//    int op = 2;
+//    vector<int> ops;
+//
+//    while(n != 1){
+//        if(n%op == 0){
+//            ops.push_back(op);
+//            n /= op;
+//        }
+//        else op++;
+//    }
+//
+//    for(int i=0; i<ops.size(); i++) cout << ops[i] << endl;
+//}
