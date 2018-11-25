da2425d1ebf08c8f54eb072d76fdc1cb299edd9a
cchloe2311 <cchloe2311@daum.net>
2018-11-26 01:45:04 +0900

    solved binary problem

---
+++ course4/binary.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    vector<int> output;
+//
+//    while(1){
+//        output.push_back(n%2);
+//        n /= 2;
+//        if(n == 0) break;
+//    }
+//
+//    for(int i=output.size()-1; i>=0; i--)
+//        cout << output[i];
+//}
