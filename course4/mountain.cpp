35b32412357afce6dd8f5a1797627786f63b57f2
cchloe2311 <cchloe2311@daum.net>
2018-11-27 00:55:28 +0900

    solved mountain problem

---
+++ course4/mountain.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    vector<int> output;
+//
+//    for(int i=1; i<=n; i++){
+//        output.push_back(i);
+//        int orgSize = output.size();
+//        for(int j=orgSize-2; j >= 0 ; j--) output.push_back(output[j]);
+//    }
+//
+//    for(int i=0; i<output.size(); i++) cout << output[i];
+//}
