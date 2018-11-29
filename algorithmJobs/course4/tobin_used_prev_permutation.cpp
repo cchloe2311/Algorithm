f34eb6382024804f42a3d9b18b3d7a993847a303
cchloe2311 <cchloe2311@daum.net>
2018-11-29 11:42:41 +0900

    solved tobin problem using prev_permutation

---
+++ algorithmJobs/course4/tobin_used_prev_permutation.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string.h>
+//
+//using namespace std;
+//
+//int main(){
+//    int n, k;
+//    cin >> n >> k;
+//    vector<int> v;
+//
+//    for(int i=0; i<k; i++) v.push_back(1);
+//    for(int i=k; i<n; i++) v.push_back(0);
+//
+//    do{
+//        for(int i=0; i<n; i++) cout << v[i];
+//        cout << endl;
+//    }while(prev_permutation(v.begin(), v.end()));
+//
+//}
