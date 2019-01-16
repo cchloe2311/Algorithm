2558297290503a49ef3c7b29c5ce18a527d046e2
cchloe2311 <cchloe2311@daum.net>
2019-01-16 14:57:45 +0900

    2019/01/16 fiexd cutTree problem but still it scored 70 because of time exceeding

--- algorithmJobs/parametric_search/cutTree.cpp
+++ algorithmJobs/parametric_search/cutTree.cpp
@@ -10,27 +10,43 @@ int getSum(int *sub, int n){
     return sum;
 }
 
+void setSub(int *tree, int *sub, int mid, int n){
+    for(int i=0; i<n; i++) {
+        int tempSum = tree[i] - mid;
+        if(tempSum > 0) sub[i] = tree[i] - mid;
+        else sub[i] = 0;
+    }
+}
+
 int main(){
     int n, m;
     cin >> n >> m;
 
     int *tree =  new int[n];
     int *sub = new int[n];
-    memset(sub, 0, sizeof(int)*n);
+    //memset(sub, 0, sizeof(int)*n);
+
+//    int tree[4];
+//    int sub[4];
 
     for(int i=0; i<n; i++) cin >> tree[i];
 
-    int h = *max_element(tree, tree+n);
+    int low = 0;
+    int high = *max_element(tree, tree+n);
+    int mid;
 
-    while(getSum(sub, n) < m){
-        h--;
-        for(int i=0; i<n; i++) {
-            int tempSum = tree[i] - h;
-            if(tempSum > 0) sub[i] = tree[i] - h;
+    while(1){
+        mid = (low+high)/2;
+
+        setSub(tree, sub, mid, n);
+
+        if(getSum(sub, n) < m) high = mid - 1;
+        else{
+            setSub(tree, sub, mid+1, n);
+            if(getSum(sub, n) < m) break;
+            else low = mid + 1;
         }
     }
 
-
-
-    cout << h;
+    cout << mid;
 }
