393efb89cf1ebadfbef80c81bf1684a1ba3039df
cchloe2311 <cchloe2311@daum.net>
2019-01-23 13:35:36 +0900

    2019/01/23 tried to solve tower problem in O(n) time complexity but it scored 90

---
+++ algorithmJobs/stack/tower_n.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <utility>
+#include <stack>
+using namespace std;
+
+int findReceiver(stack<pair<int, int>> &s, int value, int position){
+    while(!s.empty() && s.top().first < value){
+        s.pop();
+    }
+
+    int returnValue;
+    if(s.empty()) returnValue = 0;
+    else returnValue = s.top().second;
+
+    s.push({value, position});
+
+    return returnValue;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    int *towers = new int[n];
+
+    for(int i=0; i<n; i++) cin >> towers[i];
+
+    stack<pair<int, int>> s; // first == value, second == position
+
+    for(int i=0; i<n; i++){
+        cout << findReceiver(s, towers[i], i+1) << " ";
+    }
+}
