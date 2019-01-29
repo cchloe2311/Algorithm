9571fcb9714aebb16622baaed18ed2246aad264e
cchloe2311 <cchloe2311@daum.net>
2019-01-30 01:06:20 +0900

    2019/01/29 completed goodseq problem

--- algorithmJobs/Adv. recursive func/goodseq.cpp
+++ algorithmJobs/Adv. recursive func/goodseq.cpp
@@ -5,14 +5,6 @@ using namespace std;
 #define DIGITS 3
 
 bool isGoodSeq(string seq) {
-	// 이미 GoodSeq인 string에
-	// 뒤에 새로운 숫자 하나가 붙은 상태
-
-	// 좋지않은 시퀀스가 되는 케이스
-	// ~123 => ~1233 // newseq와 가장 뒤에 거를 비교
-	// ~121 => ~1212 // 뒤에서 두개랑 앞에 두개를 비교
-	// ~12312 => ~123123 // 뒤에서 세개랑 앞에 세개를 비교
-
 	for (int i = 1; i <= seq.size() / 2; i++) {
 		// substr(position, size)
 		string post = seq.substr(seq.size()-i, i);
@@ -25,8 +17,9 @@ bool isGoodSeq(string seq) {
 bool makeSeq(string seq, int n) {
 	for (int i = 1; i <= DIGITS; i++)
 		if (isGoodSeq(seq + char(48+i))) {
-			if (n != 0)
-				if(makeSeq(seq + char(48 + i), n-1)) return true;
+			if (n != 1) {
+                if (makeSeq(seq + char(48 + i), n - 1)) return true;
+            }
 			else {
 					cout << seq+char(48+i);
 					return true;
@@ -40,6 +33,5 @@ int main() {
 	int n;
 	cin >> n;
 
-	// n자리의 3진수를 만드는 것과 같음
 	makeSeq("", n);
 }
