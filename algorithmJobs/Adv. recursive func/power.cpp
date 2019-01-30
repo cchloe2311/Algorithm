2fbe7fb942e0185bf631f93694155870dc87da7a
cchloe2311 <cchloe2311@daum.net>
2019-01-30 14:36:32 +0900

    2019/01/30 tried to fix power problem but it still gets wrong answer

--- algorithmJobs/Adv. recursive func/power.cpp
+++ algorithmJobs/Adv. recursive func/power.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 #define DIV 10007
 
-long long getPower(int n, int k){
+long long getPower(int n, long long k){
     if(--k == 0) return n;
-    else return n*getPower(n, k)%DIV;
+    else return n*(getPower(n, k)%DIV)%DIV;
 }
 
 int main(){
     long long n, m;
     cin >> n >> m;
 
-    cout << getPower(n, m);
+    cout << getPower(n, m%DIV);
 }
