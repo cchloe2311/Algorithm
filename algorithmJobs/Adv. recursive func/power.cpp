b40a608d21734bbd506bbdf896cb1ebe93e2d5a9
cchloe2311 <cchloe2311@daum.net>
2019-01-30 15:44:08 +0900

    2019/01/30 tring to solve problem power

--- algorithmJobs/Adv. recursive func/power.cpp
+++ algorithmJobs/Adv. recursive func/power.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
-
+#include <utility>
+#include <math.h>
 using namespace std;
 
 #define DIV 10007
 
-long long getPower(int n, long long k){
-    if(--k == 0) return n;
-    else return n*(getPower(n, k)%DIV)%DIV;
+//long long getPower(int n, int power){
+//    if(power == 0) return 1;
+//    else return (n*getPower(n, --power))%DIV;
+//}
+
+void makeFunction(int m){
+    int power  = 0;
+
+    while(1){
+        if(m <= pow(2, power)) break;
+        power++;
+    }
+
+    int k = power > 0 ? power-1 : power;
+    cout << "2^" << k << " + ";
+
+    if(power == 0) return;
+    else makeFunction(m-pow(2, --power));
+
 }
 
 int main(){
-    long long n, m;
+    int n, m;
     cin >> n >> m;
 
-    cout << getPower(n, m%DIV);
+//    pair<int, int> function = makeFunction(m);
+    // 2^k + l으로는 안됨,, 더...
+
+    makeFunction(m);
+
+//    cout << (getPower(pow(n, 2), function.first)*getPower(n, function.second))%DIV;
 }
