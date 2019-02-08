8cd95ffe9e5f6e3e31563e8d432515ffdb648f62
cchloe2311 <cchloe2311@daum.net>
2019-02-08 18:22:28 +0900

    2019/02/08 tried to solve cardGame problem but it gets wrong answer

--- algorithmJobs/dynamic programming/cardGame.cpp
+++ algorithmJobs/dynamic programming/cardGame.cpp
@@ -15,18 +15,21 @@ int main(){
     cin >> n;
 
     int *cards = new int[n+1];
-    for(int i=1; i<=n; i++) cin >> cards[i];
+    for(int i=1; i<=n; i++) cin >> cards[i]; // 자연수 점수
 
     Count *count = new Count[n];
     count[0].seq[0] = count[0].seq[1] = 0;
 
     count[1].seq[0] = cards[1];
     count[1].seq[1] = 0;
+
     for(int i=2; i<=n; i++){
         count[i].seq[0] = count[i-2].seq[0] > count[i-2].seq[1] ? count[i-2].seq[0] : count[i-2].seq[1];
         count[i].seq[0] += cards[i];
         count[i].seq[1] = count[i-1].seq[0] + cards[i];
     }
 
-    cout << (count[n].seq[0] > count[n].seq[1] ? count[n].seq[0] : count[n].seq[1]);
+    int max = count[n].seq[0] > count[n].seq[1] ? count[n].seq[0] : count[n].seq[1];
+    if(max < count[n-1].seq[1]) max = count[n-1].seq[1];
+    cout << max;
 }
