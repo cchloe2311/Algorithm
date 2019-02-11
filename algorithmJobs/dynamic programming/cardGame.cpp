f2e101d6ad1f67fb4d908deca361be51d3375e00
cchloe2311 <cchloe2311@daum.net>
2019-02-12 00:11:43 +0900

    2019/02/12 solved cardGame problem

--- algorithmJobs/dynamic programming/cardGame.cpp
+++ algorithmJobs/dynamic programming/cardGame.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 class Count{
@@ -9,17 +10,6 @@ public:
         this->seq[1] = 0;
     }
 };
-
-int getMax(Count* count, int index){
-    int max = 0;
-
-    for(int i=0; i<=index; i++){
-        if(max < count[i].seq[0]) max = count[i].seq[0];
-        if(max < count[i].seq[1]) max = count[i].seq[1];
-    }
-
-    return max;
-}
 
 int main(){
     int n;
@@ -34,14 +24,17 @@ int main(){
     count[1].seq[0] = cards[1];
     count[1].seq[1] = 0;
 
+    int maxValue = 0;
+
     for(int i=2; i<=n; i++){
-        count[i].seq[0] = getMax(count, i-2);
+        maxValue = max(maxValue, max(count[i-2].seq[0], count[i-2].seq[1]));
+        count[i].seq[0] = maxValue;
         count[i].seq[0] += cards[i];
         count[i].seq[1] = count[i-1].seq[0] + cards[i];
     }
 
-    int max = count[n].seq[0] > count[n].seq[1] ? count[n].seq[0] : count[n].seq[1];
-    if(max < count[n-1].seq[1]) max = count[n-1].seq[1];
+    maxValue = count[n].seq[0] > count[n].seq[1] ? count[n].seq[0] : count[n].seq[1];
+    if(maxValue < count[n-1].seq[1]) maxValue = count[n-1].seq[1];
 
-    cout << max;
+    cout << maxValue;
 }
