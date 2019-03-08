612d94033aecbc6e2c59eced9a886e6902f61938
cchloe2311 <cchloe2311@daum.net>
2019-03-08 16:43:25 +0900

    2019/03/07 tried to solve binaryGraph_dfs problem but it gets wrong answer

---
+++ algorithmJobs/dfs & bfs/binaryGraph_dfs.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string.h>
+using namespace std;
+
+bool isPossible(vector<int> *graph, int n){
+    stack<int> s;
+    int *group = new int[n + 1];
+    memset(group, 0, sizeof(int) * (n + 1));
+
+    s.push(1);
+    group[1] = 1;
+
+    while(!s.empty()) {
+        int top = s.top();
+
+        for (int i = 0; i < graph[top].size(); i++) {
+            if (group[graph[top][i]] == group[top]) {
+                return false;
+            }
+            else if (group[graph[top][i]] == 0) {
+                group[graph[top][i]] = (group[top] * -1);
+                s.push(graph[top][i]);
+
+                break;
+            }
+            else if (i == (graph[top].size() - 1)) {
+                s.pop();
+            }
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> *graph = new vector<int>[n + 1];
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    if (isPossible(graph, n)) cout << "YES";
+    else cout << "NO";
+}
