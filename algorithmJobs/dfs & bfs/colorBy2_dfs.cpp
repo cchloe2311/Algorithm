4e1da1976cad9c53ebdf901e75af552a312a391d
cchloe2311 <cchloe2311@daum.net>
2019-03-08 14:59:24 +0900

    2019/03/08 solved colorBy2_dfs problem

---
+++ algorithmJobs/dfs & bfs/colorBy2_dfs.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string.h>
+using namespace std;
+
+bool isPossible(vector<int> *graph, int n){
+    stack<int> s;
+    int *colored = new int[n];
+    memset(colored, 0, sizeof(int) * n);
+
+    s.push(0);
+    colored[0] = 1;
+
+    while(!s.empty()) {
+        int top = s.top();
+
+        for (int i = 0; i < graph[top].size(); i++) {
+            if (colored[graph[top][i]] == colored[top]) {
+                return false;
+            }
+            else if (colored[graph[top][i]] == 0) {
+                colored[graph[top][i]] = (colored[top] * -1);
+                s.push(graph[top][i]);
+
+                break;
+            }
+            else if (i == (graph[top].size() - 1)) {
+                s.pop();
+            }
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> *graph = new vector<int>[n];
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    if (isPossible(graph, n)) cout << "YES";
+    else cout << "NO";
+}
