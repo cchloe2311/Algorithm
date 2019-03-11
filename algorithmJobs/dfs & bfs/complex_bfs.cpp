b46ce775a572a1dca8fb1df91cbef23fa0d82ba7
cchloe2311 <cchloe2311@daum.net>
2019-03-11 17:28:29 +0900

    2019/03/11 solved complex_bfs problem

---
+++ algorithmJobs/dfs & bfs/complex_bfs.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <algorithm>
+#include <stdlib.h>
+
+using namespace std;
+
+bool isInRange(int n, int y, int x) {
+    if (y >= 0 && y < n && x >= 0 && x < n) return true;
+    else return false;
+}
+
+int getNumOfHouse(int **map, int n, int y, int x) {
+    pair<int, int> neighbor[4] = {{-1, 0}, {0, -1},
+                                  {+1, 0}, {0, +1}};
+
+    queue<pair<int, int> > q;
+
+    int count = 0;
+
+    q.push({y, x});
+    map[y][x] = -1;
+    count++;
+
+    while (!q.empty()) {
+        pair<int, int> top = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int yPos = top.first + neighbor[i].first;
+            int xPos = top.second + neighbor[i].second;
+
+            if (isInRange(n, yPos, xPos) && (map[yPos][xPos] == 1)) {
+                q.push({yPos, xPos});
+                map[yPos][xPos] = -1;
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    vector<int> complex;
+
+    int n;
+    cin >> n;
+
+    int **map =  new int*[n];
+    for (int i = 0; i < n; i++) {
+        map[i] = new int[n];
+
+        string input;
+        cin >> input;
+
+        for (int j = 0; j < n; j++) {
+            map[i][j] = atoi(input.substr(j, 1).c_str());
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (map[i][j] == 1) {
+                complex.push_back(getNumOfHouse(map, n, i, j));
+            }
+        }
+    }
+
+    sort(complex.begin(), complex.end());
+
+    cout << complex.size() << endl;
+
+    for (int i = 0; i < complex.size(); i++) {
+        cout << complex[i] << endl;
+    }
+
+}
