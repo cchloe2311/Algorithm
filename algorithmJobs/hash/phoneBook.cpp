ff85e80397fca0c8a13cfc77d242e4d3b37a2133
cchloe2311 <cchloe2311@daum.net>
2019-03-15 20:34:52 +0900

    2019/3/15 improved phoneBook problem

--- algorithmJobs/hash/phoneBook.cpp
+++ algorithmJobs/hash/phoneBook.cpp
@@ -7,15 +7,10 @@ using namespace std;
 bool solution(vector<string> phone_book) {
     bool answer = true;
 
-    for (int i = 0; i < phone_book.size(); i++) {
-        for (int j = (i + 1); j < phone_book.size(); j++) {
-            int length = min(phone_book[i].size(), phone_book[j].size());
+    sort(phone_book.begin(), phone_book.end());
 
-            if (phone_book[i].substr(0, length) == phone_book[j].substr(0, length)) {
-                answer = false;
-                return answer;
-            }
-        }
+    for (int i = 0; i < phone_book.size() - 1; i++) {
+        if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size())) return false;
     }
 
     return answer;
