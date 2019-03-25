96b6c866ba46d501cc96fd1feb1723c6a6caf8bf
cchloe2311 <cchloe2311@daum.net>
2019-03-26 01:26:03 +0900

    2019/03/26 tsolving snake problem

--- algorithm/simulation/snake.cpp
+++ algorithm/simulation/snake.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 struct Point {
@@ -10,6 +12,11 @@ struct Point {
         this->y = y;
         this->x = x;
     }
+
+    bool operator==(const Point& rhs) {
+        if ((this->y==rhs.y) && (this->x==rhs.x)) return true;
+        else return false;
+    }
 };
 
 struct Redirection {
@@ -18,7 +25,7 @@ struct Redirection {
 };
 
 bool isGameEnded(int n, int y, int x) {
-    if ((0 <= y) && (n > y) && (0 <= x) && (n > x)) return false;
+    if ((0 < y) && (n >= y) && (0 < x) && (n >= x)) return false;
     return true;
 }
 
@@ -26,34 +33,52 @@ int main() {
     int n, k, l;
     cin >> n >> k;
 
-    Point toward[4] = {Point(0, +1), Point(-1, 0),
-                          Point(0, -1), Point(+1, 0)};
+    Point toward[4] = {Point(0, +1), Point(+1, 0),
+                          Point(0, -1), Point(-1, 0)};
 
-    Point *appleP = new Point[k];
+    vector<Point> appleP;
     for (int i = 0; i < k; i++) {
-        cin >> appleP[i].y >> appleP[i].x;
+        int y, x;
+        cin >> y >> x;
+
+        appleP.push_back(Point(y, x));
     }
 
     cin >> l;
     Redirection *redirection = new Redirection[l];
     for (int i = 0; i < l; i++) {
-        cin >> redirection[i].sec >> redirection[i].direction;
+        char direction;
+
+        cin >> redirection[i].sec >> direction;
+
+        if (direction == 'D') redirection[i].direction = +1;
+        else redirection[i].direction = -1;
     }
 
     int time = 0, direction = 0, redirectionIdx = 0;
-    Point curP = Point(0, 0);
+    Point curP = Point(1, 1);
+
+    // TODO 뱀이 자신의 몸에 부딫힌 경우!
+    // TODO 문제 이해 // 첫번째 예제의 답이 9인 이유를 모르겠음ㅜ
 
     while(true) {
         time++;
 
-        // TODO what if it exceeds the numOfitem?
-        if (time == redirection[redirectionIdx].sec) {
+        curP.y += toward[direction].y;
+        curP.x += toward[direction].x;
+
+        if ((redirectionIdx < l) && (time == redirection[redirectionIdx].sec)) {
             direction = (direction + redirection[redirectionIdx].direction) % 4;
             redirectionIdx++;
         }
 
-        curP.y += toward[direction].y;
-        curP.x += toward[direction].x;
+        auto it = find(appleP.begin(), appleP.end(), Point(curP.y, curP.x));
+        if (it != appleP.end()) {
+            curP.y += toward[direction].y;
+            curP.x += toward[direction].x;
+
+            appleP.erase(it);
+        }
 
         if (isGameEnded(n, curP.y, curP.x)) break;
     }
