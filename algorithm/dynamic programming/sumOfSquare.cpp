2b8d8691725cbc5b1a20912955b18a8695c37234
cchloe2311 <cchloe2311@daum.net>
2019-03-28 15:23:21 +0900

    2019/03/28 solved sumOfSquare problem

--- algorithm/dynamic programming/sumOfSquare.cpp
+++ algorithm/dynamic programming/sumOfSquare.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <string.h>
+#include <algorithm>
+
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
 
-    int *dp = new int[n+1];
+    long long *dp = new long long[n + 1];
+    memset(dp, 0, sizeof(long long) * (n + 1));
     dp[0] = 1;
-    int num = 1;
+    long long num = 1;
 
     for(int i=1; i<=n; i++){
-        int square = num*num;
+        long long square = num*num;
+
         if(square == i) num++;
         else square = (num-1)*(num-1);
 
-        dp[i] = dp[square]+dp[i-square];
+        long long minValue = dp[1 * 1]+dp[i- 1 * 1];
+        for (int j = 2; j < num; j++) {
+            minValue = min(minValue, dp[j * j]+dp[i- j * j]);
+        }
+
+        dp[i] = minValue;
     }
 
     cout << dp[n];
