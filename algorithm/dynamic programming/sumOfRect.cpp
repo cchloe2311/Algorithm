3b2140a0974238f880e2f1738948d950a15234b7
cchloe2311 <cchloe2311@daum.net>
2019-03-31 15:57:35 +0900

    2019/03/31 solved sumOfRect problem

--- algorithm/dynamic programming/sumOfRect.cpp
+++ algorithm/dynamic programming/sumOfRect.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-int getSum(int **rect, int a, int b, int c, int d){
-    int sum = 0;
+int getSum(int **partSum, int a, int b, int c, int d){
+    int sum = partSum[c + 1][d + 1];
 
-    for(int i=a; i<=c; i++)
-        for(int j=b; j<=d; j++){
-            sum +=rect[i][j];
-        }
+    sum -= (partSum[c + 1][b] + partSum[a][d + 1]);
+    sum += partSum[a][b];
 
     return sum;
+}
+
+void getPartSum(int **rect, int **partSum, int n, int m) {
+
+    for (int i = 1; i <= n; i++) {
+        int rowSum = 0;
+        for (int j = 1; j <= m; j++) {
+            rowSum += rect[i - 1][j - 1];
+
+            partSum[i][j] = partSum[i - 1][j] + rowSum;
+        }
+    }
 }
 
 int main(){
@@ -17,17 +28,35 @@ int main(){
     cin >> n >> m >> q;
 
     int **rect = new int*[n];
-    for(int i=0; i<n; i++) rect[i] = new int[m];
+    int **partSum = new int*[n + 1];
+    for(int i=0; i<n; i++) {
+        rect[i] = new int[m];
+        partSum[i] = new int[m + 1];
+        memset(partSum[i], 0, sizeof(int) * (m + 1));
+    }
+    partSum[n] = new int[m + 1];
+    memset(partSum[n], 0, sizeof(int) * (m + 1));
 
-    for(int i=0; i<n; i++)
-        for(int j=0; j<m; j++) cin >> rect[i][j];
+
+    for(int i=0; i<n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> rect[i][j];
+        }
+    }
+
+    getPartSum(rect, partSum, n, m);
+
+//    for (int i = 0; i < n + 1; i++) {
+//        for (int j = 0; j < m +1; j++) cout << partSum[i][j] << "\t";
+//        cout << endl;
+//    }
 
     int *output = new int[q];
     for(int i=0; i<q; i++){
         int a, b, c, d;
         cin >> a >> b >> c>> d;
 
-        output[i] = getSum(rect, a, b, c, d);
+        output[i] = getSum(partSum, a, b, c, d);
     }
 
     for(int i=0; i<q; i++) cout << output[i] << endl;
