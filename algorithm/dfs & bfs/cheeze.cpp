c430d1513663046253ed79c2094d20826e2276cd
cchloe2311 <cchloe2311@daum.net>
2019-04-01 20:45:08 +0900

    2019/04/01 solved cheeze problem

--- algorithm/dfs & bfs/cheeze.cpp
+++ algorithm/dfs & bfs/cheeze.cpp
@@ -1,77 +1,142 @@
 #include <iostream>
-#include <vector>
+#include <queue>
+#include <string.h>
 #include <utility>
 using namespace std;
 
 #define AIR 0
 #define CHEEZE 1
-#define MELTED -1
 
-class Position{
+int neighbor[4][2] = {{0, +1}, {0, -1}, {+1, 0}, {-1, 0}};
+
+int h, w;
+
+class Position {
 public:
     int y;
     int x;
+
     Position(int y, int x) {
         this->y = y;
         this->x = x;
     }
 };
 
-bool isInRange(int y, int x, int h, int w) {
+bool isInRange(int y, int x) {
     if ((y >= 0) && (y < h) && (x >= 0) && (x < w)) return true;
     else return false;
 }
 
-pair<vector<Position>, int> bfs(vector<Position> pre, int **map, int count, int h, int w) {
-    vector<Position> cur;
+vector<Position> getAirArea(int **map) {
+    vector<Position> airV;
+    queue<Position> airQ;
 
-    int neighbor[4][2] = {{0, +1}, {0, -1}, {+1, 0}, {-1, 0}};
+    int **visited = new int*[h];
+    for (int i = 0; i < h; i++) {
+        visited[i] = new int[w];
+        memset(visited[i], 0, sizeof(int) * w);
+    }
+
+    for (int i = 0; i < w; i++) {
+        if ((map[0][i] == AIR) && (visited[0][i] == 0)) {
+            airQ.push(Position(0, i));
+            airV.push_back(Position(0, i));
+
+            visited[0][i] = 1;
+        }
+
+        if ((map[h - 1][i] == AIR) && (visited[h - 1][i] == 0)) {
+            airQ.push(Position(h - 1, i));
+            airV.push_back(Position(h - 1, i));
+
+            visited[h - 1][i] = 1;
+        }
+    }
 
     for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            cout << map[i][j] << "\t";
+        if ((map[i][0] == AIR) && (visited[i][0] == 0)) {
+            airQ.push(Position(i, 0));
+            airV.push_back(Position(i, 0));
+
+            visited[i][0] = 1;
         }
-        cout << endl;
+
+        if ((map[i][w - 1] == AIR) && (visited[i][w - 1] == 0)) {
+            airQ.push(Position(i, w - 1));
+            airV.push_back(Position(i, w - 1));
+
+            visited[i][w - 1] = 1;
+        }
     }
-    cout << endl;
 
+    while(!airQ.empty()) {
+        Position front = airQ.front();
+        airQ.pop();
 
-    for (int i = 0; i < pre.size(); i++) {
         for (int j = 0; j < 4; j++) {
-            int y = pre[i].y + neighbor[j][0];
-            int x = pre[i].x + neighbor[j][1];
+            int y = front.y + neighbor[j][0];
+            int x = front.x + neighbor[j][1];
 
-            if (isInRange(y, x, h, w) && (map[y][x] == CHEEZE)) {
-                map[y][x] = MELTED;
-                cur.push_back(Position(y, x));
+            if (isInRange(y, x) && (map[y][x] == AIR) && (visited[y][x] == 0)) {
+                airQ.push(Position(y, x));
+                airV.push_back(Position(y, x));
+
+                visited[y][x] = 1;
             }
         }
     }
 
-    if (cur.empty()) return {pre, count - 1};
-    else return bfs(cur, map, ++count, h, w);
+    return airV;
+}
+
+pair<int, int> getResult(int **map) {
+    int turn = 0;
+    int preCheeze = 0;
+
+    while(true) {
+        bool isDone = true;
+        int numOfCheeze = 0;
+        vector<Position> airV = getAirArea(map);
+
+        for (int i = 0; i < airV.size(); i++) {
+            Position tempP = airV[i];
+
+            for (int j = 0; j < 4; j++) {
+                int y = tempP.y + neighbor[j][0];
+                int x = tempP.x + neighbor[j][1];
+
+                if (isInRange(y, x) && (map[y][x] == CHEEZE)) {
+                    map[y][x] = AIR;
+                    isDone = false;
+
+                    numOfCheeze++;
+                }
+            }
+        }
+
+        if (isDone) {
+            return {turn, preCheeze};
+        }
+        else {
+            turn++;
+            preCheeze = numOfCheeze;
+        }
+    }
 }
 
 int main() {
-    int h, w;
-    cin >> h >>w;
+    cin >> h >> w;
 
     int **map = new int*[h];
-    vector<Position> airPos;
-
     for (int i = 0; i < h; i++) {
         map[i] = new int[w];
 
         for (int j = 0; j < w; j++) {
             cin >> map[i][j];
-
-            if (map[i][j] == AIR) {
-                airPos.push_back(Position(i, j));
-            }
         }
     }
 
-    pair<vector<Position>, int> result = bfs(airPos, map, 1, h, w);
+    pair<int, int> result = getResult(map);
 
-    cout << result.second << endl << result.first.size();
+    cout << result.first << endl << result.second;
 }
