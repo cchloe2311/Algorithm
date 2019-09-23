7f454993e740ec41542c4700b5bdd833aa4eaac8
cchloe2311 <cchloe2311@daum.net>
2019-09-24 00:14:30 +0900

    solved longestSubstrWithoutRepeatingChar problem

--- leetcode/medium/longestSubstrWithoutRepeating.cpp
+++ leetcode/medium/longestSubstrWithoutRepeating.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,28 +11,29 @@ public:
         int index = 0;
         int max = 0;
 
-        int check[26];
-        memset(check, -1, sizeof(check));
+        map<char, int> map;
 
         bool isDone = false;
 
-        while (!isDone) {
+        while ((!isDone) && (index != s.length())) {
             for (int i = index; i < s.length(); i++) {
-                if (check[s[i] - 'a'] == -1) {
-                    check[s[i] - 'a'] = i;
+                if (i == s.length() - 1) isDone = true;
 
-                    if (i == s.length() - 1) isDone = true;
+                auto iter = map.find(s[i]);
+                if (iter == map.end()) {
+                    map.insert({s[i], i});
                 }
                 else {
                     int length = i - index;
                     max = (length > max) ? length : max;
 
-                    index = check[s[i] - 'a'] + 1;
-                    memset(check, -1, sizeof(check));
-
+                    index = (*iter).second + 1;
+                    map.clear();
                     break;
                 }
             }
+
+            max = map.size() > max ? map.size() : max;
         }
 
         return max;
@@ -39,5 +42,36 @@ public:
 
 int main() {
     Solution solution;
-    cout << solution.lengthOfLongestSubstring("pwwkew");
+    cout << solution.lengthOfLongestSubstring("nfpdmpi");
 }
+
+//int index = 0;
+//int max = 0;
+//
+//int check[26];
+//memset(check, -1, sizeof(check));
+//
+//bool isDone = false;
+//
+//while ((!isDone) && (index != s.length())) {
+//for (int i = index; i < s.length(); i++) {
+//if (i == s.length() - 1) isDone = true;
+//
+//if (s[i] < 'a' || s[i] > 'z') {
+//} else if (check[s[i] - 'a'] == -1) {
+//check[s[i] - 'a'] = i;
+//
+//if (i == s.length() - 1) isDone = true;
+//} else {
+//int length = i - index;
+//max = (length > max) ? length : max;
+//
+//index = check[s[i] - 'a'] + 1;
+//memset(check, -1, sizeof(check));
+//
+//break;
+//}
+//}
+//}
+//
+//return max;
